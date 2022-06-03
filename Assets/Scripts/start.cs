bfc591b3119099d884c0542b768b5399afa22c93
PLProtom <agent0tomek@gmail.com>
2022-06-03 11:40:11 +0200

    Timing mapy z muzyką

--- Assets/Scripts/start.cs
+++ Assets/Scripts/start.cs
@@ -7,16 +7,19 @@ public class start : MonoBehaviour
     public int isStarted = 0;
 
     public GameObject DashText;
+    public GameObject music;
+    AudioSource audioSource;
 
     void Start()
     {
-        
+        audioSource = music.GetComponent<AudioSource>();
     }
 
     private void OnTriggerEnter(Collider other)
     {
         isStarted = 1;
-        DashText.GetComponent<BottomText>().ChangeText("Map 1");
+        audioSource.Play();
+        DashText.GetComponent<BottomText>().ChangeText("Geoxor - Ephemeral");
         gameObject.SetActive(false);
     }
 }
